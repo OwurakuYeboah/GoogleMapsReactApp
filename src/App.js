7c5490bf080716b9008545e166e186964020b481
Owuraku Yeboah <owuraku.d.yeboah@gmail.com>
2019-05-01 18:32:38 -0600

    updated App.js with marker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { InfoWindow, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
@@ -7,6 +8,27 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+   state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+  
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
   render() {
     return (
       <div><Map
@@ -17,7 +39,22 @@ export class MapContainer extends Component {
          lat: -1.2884,
          lng: 36.8233
         }}
-      />
+      >
+	   <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+		
+		   <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
 	  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDygGwTT_ShpZTg2mTWJ5EF2LBsK2B7AZA&callback=initMap"
   type="text/javascript"></script></div>
     );
